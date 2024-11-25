a44d2642a23a7fa7f016d977c334dccfc36091ca
Hideo Miamoto <hideomiamoto@yandex.ru>
2024-11-25 21:29:44 +0300

    Testing

--- triangle.py
+++ triangle.py
@@ -29,4 +29,7 @@ def perimeter(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         raise ValueError("Is not a triangle")
 
+    if a + b <= c and b + c <= a and a + c <= b:
+        raise ValueError("Is not a triangle")
+
     return a + b + c
